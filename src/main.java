0e4f37399b85ac311d7663209a33fed0cc9ee543
Sofia Kostina <vip-miss@mail.ru>
2023-07-14 22:30:35 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main (String[] args) {
+        int amount = 13676;
+        int percent = 5;
+        int bonus = amount * percent / 100;
+
+        System.out.println("Итоговый бонус = " + bonus + " миль");
+    }
+}
